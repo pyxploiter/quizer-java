1ba9b3a1eba2606427a4ffca03b37458d1eb5d0b
M Asad Ali <mali.bscs15seecs@seecs.edu.pk>
2017-09-22 15:37:34 +0500

    User class test cases added

---
+++ src/test/TestQuizer.java
@@ -0,0 +1,64 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestQuizer {
+
+	@Test
+	public void testQuizer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSerializeUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSerializeQuiz() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeserializeUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeserializeQuiz() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateQuiz() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testComboBoxEventListener() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAttemptQuiz() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testQuizButtonEventListener() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAttemptGUI() {
+		fail("Not yet implemented");
+	}
+
+}
